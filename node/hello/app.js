1460c79767ad0ca8d344401312edab05aacaad91
Luan Linares <luan.miguel@rarolabs.com.br>
2020-04-24 18:56:14 -0300

    add babel

--- node/hello/app.js
+++ node/hello/app.js
@@ -1,8 +1,10 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
-app.get("/hello", function(req, res) {
+app.get("/hello", (req, res) => {
     res.status(200).json({message: "Ola NodeJs com express"})
 })
 
 app.listen(3000);
+
+module.exports = app;
